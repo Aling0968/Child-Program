c2e85ff0fb7414e3658d83df85225e7da211c711
Your Name <email@example.com>
2024-11-30 22:07:15 +0800

    P11课程列表编写，已经完成。 注意：但无法完成界面的跳转

---
+++ src/utils/data.js
@@ -0,0 +1,4 @@
+export function CreateRandomInt()
+{
+    return Math.floor(Math.random() *100);
+}
--- src/views/course/CourseGrid.vue
+++ src/views/course/CourseGrid.vue
@@ -1,83 +1,97 @@
 <script setup>
-
+import {useRouter} from "vue-router";
+import axios from "axios";
+import {ref} from "vue";
+import {CreateRandomInt} from "@/utils/data.js";
+import CourseBreadcrumb from "@/components/course/CourseBreadcrumb.vue";
+const data = ref([])
+axios.get('http://localhost:8080/system/course/list').then((res) => {
+  data.value = res.data.rows
+})
 </script>
 
 <template>
   <div>
-    <div class="breadcrumb-area relative">
-      <div class="container h-100">
-        <div class="row h-100 align-items-center">
-          <div class="col-12">
-            <div class="breadcrumb-conetnt">
-              <ul class="bread-list">
-                <li>Home</li>
-                <li><i class="icon-down-arrow-11"></i></li>
-                <li>Courses</li>
-                <li><i class="icon-down-arrow-11"></i></li>
-                <li>All Courses</li>
-              </ul>
-              <h4>All Courses</h4>
-              <p>Courses that help beginner designers become true unicorns.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- Filter Search -->
-      <div class="grid-filter-area relative">
-        <div class="container">
-          <div class="row">
-            <div class="col-12">
-              <!-- Filter -->
-              <div class="grid-filter-search relative d-flex align-items-center justify-content-between">
-                <div class="grid-filter-btn d-flex align-items-center">
-                  <div class="grid-filter-btn-list">
-                    <button class="grid-btn boxButton toggle-btn" onclick="toggleBox()"><i class="icon-mingcute_grid-line"></i>
-                      <span>Grid</span></button>
-                    <button class="list-btn"><i class="icon-cil_list"></i>
-                      <span>List</span></button>
-                  </div>
-                  <p class="mb-0">Showing 1-6 of 19 results</p>
+    <course-breadcrumb :course-count="data.length"/>
+    <div class="featured-course-4-area single-page">
+      <div class="container">
+        <div class="row">
+          <!-- Single Card -->
+          <div class="col-md-6 col-lg-4"  v-for="course in data">
+            <div class="course-card-4-area">
+              <!-- Shape -->
+              <div class="care-shape-4 wow fadeInDown" data-wow-delay="700ms" style="visibility: visible; animation-delay: 700ms; animation-name: fadeInDown;">
+                <img src="@/assets/img/bg-img/home-2/care-shap.png" alt="">
+              </div>
+              <div class="course-img-4 bg-img" style="background-image: url(@/assets/img/bg-img/home-4/shape-2.png);">
+                <div class="course-sub-title">
+                  <h6>主讲老师:{{course.teacher}}</h6>
                 </div>
-                <button class="filter-btn toggle-btn"><i class="icon-mdi_filter-outline"></i>
-                  <span>Filter</span></button>
-              </div>
+                <div class="course-title-img">
+                  <img src="@/assets/img/bg-img/home-4/c-1.png" alt="">
+                </div>
+                <!-- Shape -->
+                <div class="course-shape-4">
+                  <img src="@/assets/img/bg-img/home-4/shape-3.png" alt="">
+                </div>
 
+                <div class="course-offer-4">
+                  <div class="offer-bg-shape relative">
+                    <img src="@/assets/img/bg-img/home-4/shape-4.png" alt="">
+                  </div>
+                  <div class="offer-content-4 ">
+                    <p>-10%</p>
+                    <span>Off</span>
+                  </div>
+
+                </div>
+              </div>
+              <!-- Content -->
+              <div class="course-content-info-4">
+                <div class="course-content-rating d-flex justify-content-between align-items-center">
+                  <p><i class="icon-star1"></i> 4.5 <span>(100 次购买)</span></p>
+                  <div class="ribbon-icon">
+                    <i class="icon-icon_ribbon_alt"></i>
+                  </div>
+                </div>
+                <!-- info -->
+                <div class="course-info-meta-4 d-flex align-items-center">
+                  <p><i class="icon-Home"></i> {{CreateRandomInt()}}人正在学习</p>
+                  <p><i class="icon-book-solid-1"></i> 共{{course.lessone}}节课</p>
+                </div>
+                <h2><a href="course-details.html">{{course.title}}</a></h2>
+                <p class="course-desc-4">{{course.about}}</p>
+                <div class="auth-info-4 d-flex align-items-center">
+                  <img class="auth-img" src="@/assets/img/bg-img/home-4/c-2.png" alt="">
+                  <p>主讲老师:<a href="#">{{course.teacher}}</a></p>
+                </div>
+                <!-- Price & Cart -->
+                <div class="course-footer-4 d-flex align-items-center justify-content-between">
+                  <div class="course-price">
+                    <p>￥{{(course.price*0.9).toFixed(0)}}<span>￥{{course.price}}</span></p>
+                  </div>
+                  <div class="course-buy">
+                    <p><a href="#"><i class="icon-icon_cart_alt"></i> <span>添加到购物车</span></a>
+                    </p>
+                  </div>
+                </div>
+              </div>
 
             </div>
           </div>
-        </div>
-      </div>
 
-      <div class="row">
-        <div class="col-12">
-          <div class="searc-filed-cart-area" id="box">
-            <div class="search-filter-box">
-              <div class="single-filter-card">
-                <label for="ratingName" class="form-label">Short By Rating</label>
-                <input type="text" class="form-control" id="ratingName" placeholder="Rating" value="" required="">
-              </div>
-              <div class="single-filter-card">
-                <label for="orderName" class="form-label">Short By Order</label>
-                <input type="text" class="form-control" id="orderName" placeholder="Default" value="" required="">
-              </div>
-              <div class="single-filter-card">
-                <label for="authroName" class="form-label">Short By Author</label>
-                <input type="text" class="form-control" id="authroName" placeholder="Nothing selected" value="" required="">
-              </div>
-              <div class="single-filter-card">
-                <label for="priceName" class="form-label">Short By Price</label>
-                <input type="text" class="form-control" id="priceName" placeholder="All" value="" required="">
-              </div>
-              <div class="single-filter-card">
-                <label for="categor" class="form-label">Short By Category</label>
-                <input type="text" class="form-control" id="categor" placeholder="Nothing selected" value="" required="">
-              </div>
+
+          <div class="col-12">
+            <div class="load-btn-area text-center mt-20">
+              <a class="load-btn" href="#"><i class="icon-icon_document_alt"></i>加载更多课程</a>
             </div>
           </div>
+
         </div>
       </div>
     </div>
-  </div></template>
+  </div>
+</template>
 
 <style scoped>
 
